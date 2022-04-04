ec01855a542d723a983c78d3e425e538312d0378
Rodrigo Rivera <rodrigo.rivera@yahoo.de>
2022-04-05 00:24:13 +0400

    included confz for config management

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,3 @@
-import os
-
-from dotenv import load_dotenv
-
 from confz import ConfZ, ConfZEnvSource
 
 class FNanoApp(ConfZ):
@@ -13,18 +9,3 @@ class FNanoApp(ConfZ):
     host:str
     port:int
     CONFIG_SOURCES = ConfZEnvSource(allow_all=True)
-
-
-'''load_dotenv("./.env")
-
-
-API_USERNAME = os.environ["API_USERNAME"]
-API_PASSWORD = os.environ["API_PASSWORD"]
-
-# Auth configs.
-API_SECRET_KEY = os.environ["API_SECRET_KEY"]
-API_ALGORITHM = os.environ["API_ALGORITHM"]
-API_ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.environ["API_ACCESS_TOKEN_EXPIRE_MINUTES"]
-)  # infinity
-'''
